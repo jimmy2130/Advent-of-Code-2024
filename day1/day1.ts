efda5e980ec3d2498b5c516be41b3bf97f220301
Jimmy Chen <ccking37@gmail.com>
2024-12-01 16:09:13 +0800

    feat: complete day 1

--- day1/day1.ts
+++ day1/day1.ts
@@ -1,11 +1,38 @@
 import * as fs from 'fs';
 
 function day1() {
-	const input = fs.readFileSync('./day1.txt').toString();
-	const a = 3;
+	const input = fs.readFileSync('./day1.txt').toString().split('\n');
+	const leftList = input.map(line => Number(line.split('   ')[0])).sort();
+	const rightList = input.map(line => Number(line.split('   ')[1])).sort();
+	console.log('part 1');
+	console.log(part1(leftList, rightList));
+	console.log('part 2');
+	console.log(part2(leftList, rightList));
 	return input;
 }
-const b = 3;
-const x = 3;
 
-console.log(day1());
+function part1(leftList: number[], rightList: number[]) {
+	let ans = 0;
+	for (let i = 0; i < leftList.length; i++) {
+		const diff = Math.abs(leftList[i] - rightList[i]);
+		ans += diff;
+	}
+	return ans;
+}
+
+function part2(leftList: number[], rightList: number[]) {
+	let ans = 0;
+	for (let i = 0; i < leftList.length; i++) {
+		const target = leftList[i];
+		let frequency = 0;
+		for (let j = 0; j < rightList.length; j++) {
+			if (rightList[j] === target) {
+				frequency += 1;
+			}
+		}
+		ans += target * frequency;
+	}
+	return ans;
+}
+
+day1();
--- eslint.config.mjs
+++ eslint.config.mjs
@@ -10,6 +10,7 @@ export default tseslint.config(
 	{
 		rules: {
 			'@typescript-eslint/no-unused-vars': 'off',
+			'@typescript-eslint/prefer-for-of': 'off',
 		},
 	},
 );
